efcc81ec854631e1ffa33764a4d2985beb8e9e86
Sett Teta <stetanonsakul@gmail.com>
2023-03-18 21:20:43 +0700

    fix 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,31 +25,30 @@ export default function Home({ supplier }) {
 
   const updateSupplier = async () => {
     const response = await fetch(`/api/stock-final/supplier/${editedSupplier}`, {
-        method: "PUT", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        // redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        // serialisation
-        body: JSON.stringify(editedSupplier), // body data type must match "Content-Type" header
+      method: "PUT", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      // redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // serialisation
+      body: JSON.stringify(editedSupplier), // body data type must match "Content-Type" header
     });
     const result = await response.json();   // deserialise
     if (result.error) {
-        alert("Error: " + result.error)
+      alert("Error: " + result.error)
     }
     setData(JSON.stringify(editedSupplier))
     setEditedSupplier(null)
     window.location.reload(true);
-}
+  }
 
-  function setCurrentSupplier(id){
-    console.log(id)
-    setEditedSupplier(id)
+  function setCurrentSupplier(supplier) {
+    setEditedSupplier(supplier);
   }
 
   return (
@@ -88,7 +87,7 @@ export default function Home({ supplier }) {
                         {sup.phone}
                       </td>
                       <td>
-                        <button data-bs-toggle="modal" data-bs-target="#updateModal" onClick={() => setCurrentSupplier(sup._id)}>Update</button>
+                        <button data-bs-toggle='modal' data-bs-target='#updateModal' onClick={() => setCurrentSupplier(sup)}>Update</button>
                       </td>
                       <td>
                         <button onClick={() => deleteSupplier(sup._id)}>Delete</button>
@@ -102,40 +101,62 @@ export default function Home({ supplier }) {
         </div>
 
         <div className="modal fade" id="updateModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-lg">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Update Supplier</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="container-fluid">
-                                <form onSubmit={handleSubmit(updateSupplier)}>
-                                    <div className="row d-flex justify-content-start">
-                                        <div className="col-md-7 ">
-                                            <label htmlFor="title" className="col-form-label">Name:</label>
-                                            <input className="form-control" id="title" {...register("name", { required: false })} />
-                                        </div>
-                                        <div className="col-md-5">
-                                            <label htmlFor="link" className="col-form-label">Address:</label>
-                                            <input className="form-control" id="address" {...register("address", { required: false })} ></input>
-                                        </div>
-                                        <div className="col-md-4">
-                                            <label htmlFor="dateOfUpload" className="col-form-label">Phone Number:</label>
-                                            <input className="form-control" type="phone" id="phone" {...register("phone", { required: false })} />
-                                        </div>
-                                    </div>
-
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="submit" className="btn btn-primary">Update</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Update Supplier</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <div className="container-fluid">
+                  <form onSubmit={handleSubmit(updateSupplier)}>
+                    <div className="row d-flex justify-content-start">
+                      <div className='col-md-7 '>
+                        <label htmlFor='title' className='col-form-label'>
+                          Name:
+                        </label>
+                        <input
+                          className='form-control'
+                          id='title'
+                          defaultValue={editedSupplier?.name || ''}
+                          {...register('name', { required: false })}
+                        />
+                      </div>
+                      <div className='col-md-5'>
+                        <label htmlFor='link' className='col-form-label'>
+                          Address:
+                        </label>
+                        <input
+                          className='form-control'
+                          id='address'
+                          defaultValue={editedSupplier?.address || ''}
+                          {...register('address', { required: false })}
+                        ></input>
+                      </div>
+                      <div className='col-md-4'>
+                        <label htmlFor='dateOfUpload' className='col-form-label'>
+                          Phone Number:
+                        </label>
+                        <input
+                          className='form-control'
+                          type='phone'
+                          id='phone'
+                          defaultValue={editedSupplier?.phone || ''}
+                          {...register('phone', { required: false })}
+                        />
+                      </div>
                     </div>
+
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="submit" className="btn btn-primary">Update</button>
+                    </div>
+                  </form>
                 </div>
+              </div>
             </div>
+          </div>
+        </div>
 
       </main>
     </>
