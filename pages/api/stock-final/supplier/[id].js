c4c7ddfc452f39d51bf94b31e8df839f99de0fb5
Sett Teta <stetanonsakul@gmail.com>
2023-03-18 18:19:35 +0700

    fix 2

--- pages/api/stock-final/supplier/[id].js
+++ pages/api/stock-final/supplier/[id].js
@@ -1,5 +1,5 @@
 import dbConnect from "../../../../lib/dbConnect"
-import Supplier from "../../../../models/supplier"
+import Supplier from "../../../../models/Supplier"
 
 export default async function handler(req, res) {
     await dbConnect()
