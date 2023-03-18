4e813d1302c98a1637f0967d1eb201604c400956
Sett Teta <stetanonsakul@gmail.com>
2023-03-18 20:50:52 +0700

    update imple

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,50 @@
 import Head from 'next/head'
 import Header from '../components/header'
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 
 export default function Home({ supplier }) {
 
+  const { register, handleSubmit } = useForm();
+  const [editedVideo, setEditedVideo] = useState(null);
+
+
   function deleteSupplier(id) {
     const confirmed = window.confirm("Are you sure you want to delete this supplier?");
     if (confirmed) {
-        fetch(`/api/stock-final/supplier/${id}`, {
-            method: "DELETE"
+      fetch(`/api/stock-final/supplier/${id}`, {
+        method: "DELETE"
+      })
+        .then(res => res.json())
+        .then(data => {
+          window.location.reload(false);
         })
-            .then(res => res.json())
-            .then(data => {
-                window.location.reload(false);
-            })
     }
+  }
+
+  const updateSupplier = async () => {
+    const response = await fetch(`/api/stock-final/supplier/${editedVideo._id}`, {
+        method: "PUT", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // serialisation
+        body: JSON.stringify(editedVideo), // body data type must match "Content-Type" header
+    });
+    const result = await response.json();   // deserialise
+    if (result.error) {
+        alert("Error: " + result.error)
+    }
+    setData(JSON.stringify(editedVideo))
+    setEditedVideo(null)
+    window.location.reload(true);
 }
 
   return (
@@ -53,6 +83,9 @@ export default function Home({ supplier }) {
                         {sup.phone}
                       </td>
                       <td>
+                        <button data-bs-toggle="modal" data-bs-target="#updateModal" onClick={() => setEditedVideo(sup._id)}>Update</button>
+                      </td>
+                      <td>
                         <button onClick={() => deleteSupplier(sup._id)}>Delete</button>
                       </td>
                     </tr>
@@ -62,6 +95,42 @@ export default function Home({ supplier }) {
             </tbody>
           </table>
         </div>
+
+        <div className="modal fade" id="updateModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Update Supplier</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="container-fluid">
+                                <form onSubmit={handleSubmit(updateSupplier)}>
+                                    <div className="row d-flex justify-content-start">
+                                        <div className="col-md-7 ">
+                                            <label htmlFor="title" className="col-form-label">Name:</label>
+                                            <input className="form-control" id="title" {...register("name", { required: false })} />
+                                        </div>
+                                        <div className="col-md-5">
+                                            <label htmlFor="link" className="col-form-label">Address:</label>
+                                            <input className="form-control" id="address" {...register("address", { required: false })} ></input>
+                                        </div>
+                                        <div className="col-md-4">
+                                            <label htmlFor="dateOfUpload" className="col-form-label">Phone Number:</label>
+                                            <input className="form-control" type="phone" id="phone" {...register("phone", { required: false })} />
+                                        </div>
+                                    </div>
+
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" className="btn btn-primary">Update</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
       </main>
     </>
